f6cef5883c2efc525c40959e7befc5533a6a517a
Frozenheart32 <alexroid32@gmail.com>
2025-04-28 21:04:29 +0300

    Add Project template

---
+++ Source/BlueprintSpeedTest/BlueprintSpeedTestGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BlueprintSpeedTestGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABlueprintSpeedTestGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABlueprintSpeedTestGameMode();
+};
+
+
+
